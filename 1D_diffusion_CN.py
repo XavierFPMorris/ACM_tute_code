ca7edac7514d5259cae419d4ade6ecb0a75bd2b1
XFPM <xavier@drmorris.com.au>
2021-03-14 15:48:12 +1100

    initial commit

---
+++ 1D_diffusion_CN.py
@@ -0,0 +1,63 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import time
+
+Nx = 100
+Nt = 1000
+dt = 0.1
+Ln = 1
+
+num_iter = 1000*3
+
+x = np.linspace(0, Ln, Nx + 1)
+t = np.linspace(0, dt*Nt, Nt + 1)
+
+dx = x[2]-x[1]
+dt = t[2]-t[1]
+
+
+temp = np.ones(len(x)-1)
+
+L = np.eye(len(x))*-2 + np.diag(temp, 1) + np.diag(temp,-1)
+
+L[0][-1] = 1
+L[-1][0] = 1
+
+#u = np.zeros(Nx+1)
+
+u_n = np.zeros((num_iter, Nx+1))
+
+[val, vec] = np.linalg.eig(L)
+
+val[::-1].sort()
+
+#plt.scatter([i for i in range(np.size(val))] ,val)
+#plt.show()
+
+u = np.sin(x)
+u = np.exp(-(x-Ln/2)**2/0.01)/10
+u_n[0,:] = u
+
+mn = np.mean(u)
+
+# plt.plot(x,u)
+# plt.show()
+
+for i in range(1,num_iter):
+    #u_n[i,:] = (1-dt/2*L) @ np.linalg.inv(1+dt/2*L) @ u
+    u_n[i,:] = np.linalg.solve((1/dt + L/2), (1/dt-L/2)@u)
+    u = u_n[i,:]
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+
+def animate(i):
+    if(i%15==0):
+        ax1.clear()
+        ax1.plot(x,u_n[i,:])
+        ax1.plot([0,1], [mn,mn])
+        plt.ylim([0,1])
+
+ani = FuncAnimation(fig, animate, frames = list(range(0,num_iter)), interval = 1)
+plt.show()
